19f9f454e0777d851f26d14df0c7984267c57015
Christian Ehrhardt <christian.ehrhardt@canonical.com>
2019-06-05 16:18:41 +0200

    test: fix test-qxl-parsing on ppc64el and armhf
    On ppc64el and armhf the handling of "1 << mem_info.memslot_id_shift"
    will end up beign a zero which breaks the test.
    
    Marking the implicit value 1 as a 64 bit value (to match the uint64_t
    target) fixes the issue.
    
    Fixes #31
    
    Signed-off-by: Christian Ehrhardt <christian.ehrhardt@canonical.com>
    Acked-by: Frediano Ziglio <fziglio@redhat.com>

--- server/tests/test-qxl-parsing.c
+++ server/tests/test-qxl-parsing.c
@@ -96,7 +96,7 @@ static void test_memslot_invalid_slot_id(void)
     RedMemSlotInfo mem_info;
     init_meminfo(&mem_info);
 
-    memslot_get_virt(&mem_info, 1 << mem_info.memslot_id_shift, 16, 0);
+    memslot_get_virt(&mem_info, UINT64_C(1) << mem_info.memslot_id_shift, 16, 0);
 }
 
 static void test_memslot_invalid_addresses(void)
